fe9318df91fb4f3a3a8500facb3f0094b2e4fb5a
shtolfensie <phillipstolfa@gmail.com>
2021-03-29 17:19:57 +0200

    Add math library interface - create function stubs in mathfunc and interpreter

--- src/pkg/interpreter/interpreter.go
+++ src/pkg/interpreter/interpreter.go
@@ -1,1 +1,22 @@
 package interpreter
+
+import (
+	"errors"
+	"fmt"
+)
+
+type SyntaxError struct {
+	index int
+}
+
+func (se *SyntaxError) Error() string {
+	return fmt.Sprintf("Invalid syntax at index: %d.", se.index)
+}
+
+func Eval(input string) (float64, error)
+
+// can return the index of invalid syntax,
+// so that it can be highlighted in the GUI
+func Parse(input string) (*TreeNode, error)
+
+func Interpret(root *TreeNode) (float64, error)
