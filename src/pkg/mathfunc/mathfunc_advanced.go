c3c9d77e1fd09256c214a3b756017234a3f541a8
Tomáš Martykán <t.martykan@gmail.com>
2021-04-12 22:08:20 +0200

    Math functions

---
+++ src/pkg/mathfunc/mathfunc_advanced.go
@@ -0,0 +1,10 @@
+package mathfunc
+
+func Power(base float64, exponent float64) (float64, error) {
+	// exponent has to be a natural number (including 0?)
+	return 0, nil
+}
+
+func Root(x float64, n float64) (float64, error) {
+	return 0, nil
+}
