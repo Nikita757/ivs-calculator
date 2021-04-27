fed4b1cb53c5dc513ea7fd938140e203b50733cb
Nichita Gutu <nichitagutu@localhost.localdomain>
2021-04-27 20:20:59 +0200

    Long-awaited almost fully working parser

--- src/pkg/interpreter/interpreter.go
+++ src/pkg/interpreter/interpreter.go
@@ -1,9 +1,11 @@
 package interpreter
 
 import (
-	"errors"
 	"fmt"
 	"ivs-calculator/pkg/mathfunc"
+	"log"
+	"strconv"
+	"unicode"
 )
 
 type SyntaxError struct {
@@ -12,6 +14,22 @@ type SyntaxError struct {
 
 func (se *SyntaxError) Error() string {
 	return fmt.Sprintf("Invalid syntax at index: %d.", se.index)
+}
+
+var operOrder = map[string]struct {
+	prec   int
+	rAssoc bool
+}{
+	"-": {2, false},
+	"+": {2, false},
+	"*": {3, false},
+	"/": {3, false},
+	"%": {3, false},
+	"√": {4, true},
+	"!": {4, true},
+	"^": {4, true},
+	"m": {5, true},
+	"p": {5, true},
 }
 
 func Eval(input string) (float64, error)
@@ -102,4 +120,234 @@ func Interpret(root *TreeNode) (float64, error) {
 	} else {
 		return 0, fmt.Errorf("invalid token type: %d", root.token.tokenType)
 	}
+}
+
+func toSlice(in string) ([]string, []int) {
+	outSlice := make([]string, 0)
+	wrongSynt := make([]int, 0)
+	brackPos := make([]int, 0)
+	absPos := make([]int, 0)
+	openedBracket := false
+	openedAbs := false
+	consNum := false
+	number := ""
+	for i, tokenRune := range in {
+		token := string(tokenRune)
+		if token == "(" || token == ")" || token == "+" || token == "-" || token == "*" || token == "/" || token == "!" || token == "^" || token == "√" || token == "|" || token == "%" {
+			if consNum {
+				consNum = false
+				outSlice = append(outSlice, number)
+				number = ""
+			}
+			if token == "|" {
+				if openedAbs {
+					openedAbs = false
+					absPos = absPos[:len(absPos)-1]
+				} else {
+					openedAbs = true
+					absPos = append(absPos, i)
+				}
+			}
+
+			if token == "(" {
+				openedBracket = true
+				brackPos = append(brackPos, i)
+			}
+			if token == ")" {
+				if !openedBracket {
+					wrongSynt = append(wrongSynt, i)
+				} else {
+					brackPos = brackPos[:len(brackPos)-1]
+					openedBracket = false
+				}
+			}
+
+			outSlice = append(outSlice, token)
+		} else if unicode.IsDigit(tokenRune) || (consNum && (token == "," || token == ".")) {
+			if token == "," || token == "." {
+				number += "."
+				continue
+			}
+
+			number += token
+			consNum = true
+		} else {
+			wrongSynt = append(wrongSynt, i)
+		}
+	}
+	if len(brackPos) != 0 {
+		for _, pos := range brackPos {
+			wrongSynt = append(wrongSynt, pos)
+		}
+	}
+	if len(absPos) != 0 {
+		for _, pos := range absPos {
+			wrongSynt = append(wrongSynt, pos)
+		}
+	}
+
+	if consNum {
+		outSlice = append(outSlice, number)
+	}
+
+	return outSlice, wrongSynt
+}
+
+func inToPost(input []string) ([]string, int) {
+	post := make([]string, 0)
+	stack := make([]string, 0)
+	afterOpPar := false
+	lastDig := false
+	for i, token := range input {
+		switch token {
+		case "(":
+			lastDig = false
+			stack = append(stack, token)
+		case ")":
+			lastDig = false
+			if len(stack) == 0 {
+				return nil, i
+			}
+			for {
+				operator := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				if operator == "(" {
+					break
+				}
+				post = append(post, operator)
+				afterOpPar = true
+			}
+		case "+", "-", "/", "*", "%", "^", "!", "√":
+			curOp := token
+			if i == 0 && curOp == "-" {
+				curOp = "m"
+			} else if i == 0 && curOp == "+" {
+				curOp = "p"
+			} else if len(stack) > 0 {
+				lastOp := stack[len(stack)-1]
+
+				if !afterOpPar {
+					if lastOp != ")" && curOp == "-" && lastOp != "p" && lastOp != "m" && !lastDig {
+						curOp = "m"
+					}
+
+					if lastOp != ")" && curOp == "+" && lastOp != "p" && lastOp != "m" && !lastDig {
+						curOp = "p"
+					}
+				}
+
+				for {
+					if !operOrder[curOp].rAssoc && operOrder[curOp].prec <= operOrder[lastOp].prec || operOrder[curOp].rAssoc && operOrder[curOp].prec < operOrder[lastOp].prec {
+						operator := stack[len(stack)-1]
+						stack = stack[:len(stack)-1]
+						post = append(post, operator)
+
+						if len(stack) == 0 {
+							break
+						} else {
+							lastOp = stack[len(stack)-1]
+						}
+					} else {
+						break
+					}
+				}
+			}
+
+			stack = append(stack, curOp)
+			if token != "!" {
+				lastDig = false
+			}
+			afterOpPar = false
+		default:
+			lastDig = true
+			post = append(post, token)
+		}
+	}
+
+	for len(stack) > 0 {
+		post = append(post, stack[len(stack)-1])
+		stack = stack[:len(stack)-1]
+	}
+	return post, -1
+}
+
+func toTreeOper(stack []*TreeNode, token string) []*TreeNode {
+	var (
+		t *Token
+		l *TreeNode
+		r *TreeNode
+	)
+	switch token {
+	case "+", "-", "/", "*", "^", "%":
+		l = stack[len(stack)-2]
+		r = stack[len(stack)-1]
+		log.Println(l, r)
+	case "!", "√":
+		l = stack[len(stack)-1]
+		r = nil
+	}
+
+	if token == "%" {
+		t = NewToken(OPERATOR, "mod", 0.0)
+	} else if token == "^" {
+		t = NewToken(OPERATOR, "pow", 0.0)
+	} else if token == "√" {
+		t = NewToken(OPERATOR, "root", 0.0)
+	} else if token == "!" {
+		t = NewToken(OPERATOR, "fac", 0.0)
+	} else if token == "m" {
+		unT := NewToken(NUMBER, "-1", -1.0)
+		unN := NewNode(unT)
+		l := stack[len(stack)-1]
+		t := NewToken(OPERATOR, "*", 0.0)
+		n := NewParent(t, l, unN)
+		stack[len(stack)-1] = n
+
+		return stack
+	} else if token == "p" {
+		unT := NewToken(NUMBER, "1", 1.0)
+		unN := NewNode(unT)
+		l := stack[len(stack)-1]
+		t := NewToken(OPERATOR, "*", 0.0)
+		n := NewParent(t, l, unN)
+		stack[len(stack)-1] = n
+
+		return stack
+	} else {
+		t = NewToken(OPERATOR, token, 0.0)
+	}
+	n := NewParent(t, l, r)
+	if r == nil {
+		stack[len(stack)-1] = n
+
+		return stack
+	}
+
+	stack = stack[:len(stack)-1]
+	stack[len(stack)-1] = n
+
+	return stack
+}
+
+func postToTree(post []string) *TreeNode {
+	stack := make([]*TreeNode, 0)
+
+	for _, token := range post {
+		switch token {
+		case "+", "-", "/", "*", "^", "!", "%", "√", "m", "p":
+			stack = toTreeOper(stack, token)
+		default:
+			fl, err := strconv.ParseFloat(token, 64)
+			if err != nil {
+				log.Println("error at: ", token)
+			}
+
+			t := NewToken(NUMBER, token, fl)
+			n := NewNode(t)
+			stack = append(stack, n)
+			log.Println(n)
+		}
+	}
+
+	return stack[0]
 }
