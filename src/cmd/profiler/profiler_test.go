b3a26a84d474268ff59775a03624ff905433f3e8
vazce <>
2021-04-28 16:58:28 +0200

    Updated profiler

--- src/cmd/profiler/profiler_test.go
+++ src/cmd/profiler/profiler_test.go
@@ -1,11 +1,16 @@
-package profiler
+package main
 
 import (
 	"testing"
-	"math/rand"
+	"bufio"
+	"io"
+    "os"
+	"strings"
+	"strconv"
+	"log"
 )
 
-func TestStandardDeviation(t *testing.T) {
+func TestSD(t *testing.T) {
 	numbers := []float64{321, 43, 400, 10, 143, 400, 500, 640, 320, 4052}
 	
 	total := StandardDeviation(numbers)
@@ -14,13 +19,55 @@ func TestStandardDeviation(t *testing.T) {
     }
 }
 
-func BenchmarkStandardDeviation(b *testing.B) {
+func benchmarkSD(file *os.File, b *testing.B) {
+	reader := bufio.NewReader(file)
+	text := ""
+	for { 
+		str, err := reader.ReadString('\n')
+		text += str
+		if (err == io.EOF) {
+			break;
+		}
+	}
+	s_numbers := strings.Fields(text)
+	f_numbers := make([]float64, len(s_numbers))
+
+	for i := 0; i < len(f_numbers); i++ {
+		f_numbers[i], _ = strconv.ParseFloat(s_numbers[i], 64)
+	}
+
 	for i := 0; i < b.N; i++ {
-		numbers := []float64{
-			float64(rand.Int()), float64(rand.Int()), float64(rand.Int()),
-			float64(rand.Int()), float64(rand.Int()), float64(rand.Int()),
-			float64(rand.Int()), float64(rand.Int()), float64(rand.Int()),
-			float64(rand.Int())}
-		StandardDeviation(numbers)
-    }	
+		StandardDeviation(f_numbers)
+	}
+}
+
+func BenchmarkSD10(b *testing.B) {
+	f, err := os.Open("./testdata10")
+	if err != nil {
+        log.Fatal(err)
+    }
+	
+	defer f.Close()
+	benchmarkSD(f, b)
+	
+}
+
+func BenchmarkSD100(b *testing.B) {
+	f, err := os.Open("./testdata100")
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	defer f.Close()
+	benchmarkSD(f, b)
+}
+
+func BenchmarkSD1000(b *testing.B) {
+	f, err := os.Open("./testdata1000")
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	defer f.Close()
+	benchmarkSD(f, b)
 }
